e650e06e800a9d56e948d9a9d5b06afff844817f
surviveRuins <surviveRuins@proton.me>
2025-12-13 15:20:15 +0100

    Simple Python websocket example code that successfully connects to debug server

---
+++ horizon.py
@@ -0,0 +1,22 @@
+import websocket
+
+def on_message(ws, message):
+    print(f"Received message: {message}")
+
+def on_error(ws, error):
+    print(f"Encountered error: {error}")
+
+def on_close(ws, close_status_code, close_msg):
+    print("Connection closed")
+
+def on_open(ws):
+    print("Connection opened")
+    ws.send("Hello, Server!")
+
+if __name__ == "__main__":
+    ws = websocket.WebSocketApp("wss://echo.websocket.org",   # wss:// connects only on https and ws:// connects only on http
+                                on_message=on_message,
+                                on_error=on_error,
+                                on_close=on_close)
+    ws.on_open = on_open
+    ws.run_forever()
