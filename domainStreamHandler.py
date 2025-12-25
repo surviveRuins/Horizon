84fa636eaa671b3746f5a11ef8ef7a8e66fad37e
surviveRuins <surviveRuins@proton.me>
2025-12-25 18:43:26 +0100

    Implemented function for combo squatting detection, not implemented in general workflow or as part of CLI(flag etc)

--- domainStreamHandler.py
+++ domainStreamHandler.py
@@ -8,6 +8,11 @@ from rapidfuzz.distance import DamerauLevenshtein
 
 monitoredDomainsList = []
 counter = 0
+comboSwappingDetectionTreshold = 4; # 1: flag any domain that has the string of the secondLevelDomain in it
+                                    # 2: flag any domain that "secondLevelDomain-" (or _) in it. example-login.com will be flagged examplelogin.com will not be flagged for monitored domain example.com
+                                    # 3: flag any domain that has "-secondLevelDomain" (or _) in it. login-example-uber.com will be flagged, example-login.com will not be flagged
+                                    # 4: flag any domain that has "-secondLevelDomain.topLevelDomain" (or _) in it(meaning it has to end on our Fully qualified monitored domain.
+                                    #    login-example.com will be flagged, login-example-uber.com will not be flagged
 
 with open(sys.argv[1], "r") as file:
     for monitoredDomain in file:
@@ -17,9 +22,26 @@ def detectComboSwapping(monitoredDomain, domain):
     # Assumption for our list of monitored domains from the file given by the user in sys.argv[1]: One domain in format main.tld per line: 
     # That means we need to seperate the `main` part as that is the only relevant thing for comboswapping: paypal.com --> paypal --> now we can detect if the streamIngest() data contains paypal as a signal for comboswapping
     # In paypal.com the `.com` is called TLD(Top Level Domain) and the `paypal` is called SLD(Second Level Domain)
+    
     secondLevelDomain = monitoredDomain.split(".")[0]
-    if secondLevelDomain in domain:
-        print(domain)
+
+    match(comboSwappingDetectionTreshold):
+        case 1:
+            if secondLevelDomain in domain:
+                print(domain)
+        case 2:
+            if f"{secondLevelDomain}-" in domain or f"{secondLevelDomain}_" in domain:
+                print(domain)
+        case 3:
+            if f"-{secondLevelDomain}" in domain or f"_{secondLevelDomain}" in domain:
+                print(domain)
+        case 4:
+            if f"-{monitoredDomain}" in domain or f"_{monitoredDomain}" in domain:
+                print(domain)
+
+def detectTLDSquatting(monitoredDomain, domain):
+    print(monitoredDomain)
+            
 
 
 
