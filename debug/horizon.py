a17ffb9ee2826cb801f28ce6fc2c809c491c83b6
surviveRuins <surviveRuins@proton.me>
2025-12-26 20:23:56 +0100

    Tested and fixed TLD squatting detection and added subdomain Squatting detection, also fixing branch and git repor...

--- debug/horizon.py
+++
@@ -1,5 +0,0 @@
-import websocketConnection  # omit the .py, this is my websocketConnection.py file. It integrates as a module and I kind of behaves like a class
-                            # You can call its functions in the schema: myModule.myFunction()
-
-if __name__ == "__main__": 
-    websocketConnection.connect()
