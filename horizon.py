5f38a6fa2296af42e7de9f5855235ea6a9e447a3
surviveRuins <surviveRuins@proton.me>
2026-01-06 02:54:19 +0100

    Added a CLI banner that is displayed at the top everytime Horizon is run

--- horizon.py
+++ horizon.py
@@ -1,5 +1,16 @@
 import websocketConnection  # omit the .py, this is my websocketConnection.py file. It integrates as a module and I kind of behaves like a class
                             # You can call its functions in the schema: myModule.myFunction()
 
+banner = """\033[31m
+  _    _            _                
+ | |  | |          (_)               
+ | |__| | ___  _ __ _ _______  _ __  
+ |  __  |/ _ \\| '__| |_  / _ \\| '_ \\ 
+ | |  | | (_) | |  | |/ / (_) | | | |
+ |_|  |_|\\___/|_|  |_/___\\___/|_| |_|
+
+\033[0m"""
+
 if __name__ == "__main__": 
+    print(banner)
     websocketConnection.connect()
