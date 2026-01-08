52271979a773a06ae71e5dce5695b2eee7d40458
surviveRuins <surviveRuins@proton.me>
2026-01-08 23:36:27 +0100

    Removed weird Typer Autocomplete from the command line options of the Horizon

--- commandLineLogic.py
+++ commandLineLogic.py
@@ -6,15 +6,19 @@ import monitoredDomainsFileReader
 # Global variable because I don't want to pass this all the way trough the call stack, don't want to open the file for every new domain and don't want to overengineer right now
 global_monitoredDomainsList = []
 
-def initCLI():
-    typer.run(parseFlagsAndArgs)
+app = typer.Typer(no_args_is_help=True, add_completion=False)
 
+def initCLI():
+    app()
+
+@app.command()
 def parseFlagsAndArgs(monitored_domains_list: str = "", monitored_domain: str = ""):
     global global_monitoredDomainsList # This is the way to grab a global variable in python, if you don't do this it will not be written to the global var defined above
 
     if(monitored_domains_list):
         global_monitoredDomainsList = monitoredDomainsFileReader.readFile(monitored_domains_list)
         websocketConnection.connect()
+
     print("Invalid command provided. Please run again with --help")
 
 def getMonitoredDomainsList():
