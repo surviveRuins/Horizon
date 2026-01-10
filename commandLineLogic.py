21f5b7419ef9ce69a8a53a26cd64dd0572f7ba99
surviveRuins <surviveRuins@proton.me>
2026-01-10 01:54:48 +0100

    Added documentation for -cM(combosquatting mode) flag in --help

--- commandLineLogic.py
+++ commandLineLogic.py
@@ -6,18 +6,23 @@ import monitoredDomainsFileReader
 # Global variable because I don't want to pass this all the way trough the call stack, don't want to open the file for every new domain and don't want to overengineer right now
 global_monitoredDomainsList = []
 
-app = typer.Typer(no_args_is_help=True, add_completion=False)
+app = typer.Typer(no_args_is_help=True, add_completion=False, rich_markup_mode="markdown")
 
 def initCLI():
     app()
 
-abc = "hey\n\nho"
-combo_squatting_mode_help_message = "The mode you want to operate the combosquatting detection in:\n\n1: a\n\n2: b\n\n3:c\n\n4:d\n\n5:e"
+combo_squatting_mode_help_message = """
+The mode you want to operate the combosquatting detection in:
+    \n\n- 1: flag any domain that has the string of the secondLevelDomain in it
+    \n\n- 2: flag any domain that "secondLevelDomain-" or "\\-secondLevelDomain" in it. example-login.com,login-example.com will be flagged examplelogin.com will not be flagged for monitored domain example.com. This is basicially a combination of 3 and 4 and consists of a filter that is less strict
+    \n\n- 3: flag any domain that "secondLevelDomain-" in it. example-login.com will be flagged examplelogin.com will not be flagged for monitored domain example.com
+    \n\n- 4: flag any domain that has "-secondLevelDomain" in it. login-example-uber.com and login-examplefr.com will be flagged, example-login.com will not be flagged
+    \n\n- 5: flag any domain that has "-secondLevelDomain.topLevelDomain" in it(meaning it has to end on our Fully qualified monitored domain. login-example.com will be flagged, login-example-uber.com won't be flagged
+"""
 
 @app.command()
 def monitor(
         monitored_domains_list: str = typer.Option(None, "-dL", help="List of domains you want to monitor for domain squatting attemps"),
-        #monitored_domains: str = typer.Option(None, "-d", help="Domain you want to monitor for domain squatting attemps, can be specified multiple times"),
         monitored_domains: str = typer.Option(None, "-d", help="Domain you want to monitor for domain squatting attemps, can be specified multiple times"),
         combo_squatting_mode: str = typer.Option(None, "-cM", help=combo_squatting_mode_help_message),
 ):
